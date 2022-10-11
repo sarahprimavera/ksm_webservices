5d9afbaa8d448e64fb09f013fd7c91f5c774188b
Krikor Astour <ararad.seller@gmail.com>
2022-10-10 22:15:52 -0700

    createUser model implemented

--- webservice_finalProject/Client/app/model/model.php
+++ webservice_finalProject/Client/app/model/model.php
@@ -1,1 +1,20 @@
 <?php
+
+class model extends Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function createUser($username, $password, $phone, $email){
+        $this->query('INSERT INTO user(name, phone_num, email, password) VALUES (:name, :password, :email, :phone)');
+        $this->bind('name', $username);
+        $this->bind('password', $password);
+        $this->bind('email', $email);
+        $this->bind('phone', $phone);
+
+        return $this->execute();
+    }
+
+}
