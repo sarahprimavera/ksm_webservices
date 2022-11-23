195316523de541f7b426444ba44f026fbd017f06
Krikor Astour <ararad.seller@gmail.com>
2022-11-22 19:47:32 -0800

    basic home page

--- webservice_finalProject/Client/app/views/home.php
+++ webservice_finalProject/Client/app/views/home.php
@@ -1,7 +1,29 @@
-<?php require APPROOT . '/views/includes/header.php'; ?> 
+<?php require APPROOT . '\views\includes\header.php';?>
+<?php require APPROOT . '\views\includes\navbar.php';?>
     
+    <br>
     <form action="TestParse.php" method="post" enctype="multipart/form-data">
-   <label for="file">Filename:</label> <input type="file" name="file" id="file"/>
-<input type="submit" value="Submit">
-</form>
+        <!-- Example single danger button -->
+      
+        <label class="form-label" for="customFile">Upload Text File</label>
+        <input type="file" class="form-control" id="customFile" />
+        <div>
+            <h3>From - Language</h3>
+        <select name="original_language">
+              <option value="en" selected>English</option>
+              <option value="fr">French</option>
+              <option value="sp">Spanish</option>
+              <option value="it">Italian</option>
+            </select> <br><br>
+</div>
+<div>
+            <h3>To - Language</h3>
+        <select name="target_language">
+              <option value="en" selected>English</option>
+              <option value="fr">French</option>
+              <option value="sp">Spanish</option>
+              <option value="it">Italian</option>
+            </select> <br><br>
+</div>
+    </form>
 <?php require APPROOT . '/views/includes/footer.php'; ?>
