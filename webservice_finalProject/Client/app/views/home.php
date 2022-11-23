a314ea64d15ade64d40d9c7082ded9d0702b55f7
Krikor Astour <ararad.seller@gmail.com>
2022-11-22 19:53:48 -0800

    added some css

--- webservice_finalProject/Client/app/views/home.php
+++ webservice_finalProject/Client/app/views/home.php
@@ -2,28 +2,31 @@
 <?php require APPROOT . '\views\includes\navbar.php';?>
     
     <br>
-    <form action="TestParse.php" method="post" enctype="multipart/form-data">
-        <!-- Example single danger button -->
-      
-        <label class="form-label" for="customFile">Upload Text File</label>
-        <input type="file" class="form-control" id="customFile" />
-        <div>
-            <h3>From - Language</h3>
-        <select name="original_language">
-              <option value="en" selected>English</option>
-              <option value="fr">French</option>
-              <option value="sp">Spanish</option>
-              <option value="it">Italian</option>
-            </select> <br><br>
+    <div class="d-flex justify-content-center">
+        <form action="TestParse.php" method="post" enctype="multipart/form-data">
+            <!-- Example single danger button -->
+        
+            <label class="form-label" for="customFile">Upload Text File</label>
+            <input type="file" class="form-control" id="customFile" />
+            <div>
+                <label>From - Language</label>
+                 <select name="original_language" style="margin: 20px">
+                <option value="en" selected>English</option>
+                <option value="fr">French</option>
+                <option value="sp">Spanish</option>
+                <option value="it">Italian</option>
+                </select> <br><br>
+            </div>
+            <div>
+                <label>To - Language</label>
+                <select name="target_language" style="margin-left: 40px">
+                <option value="en" selected>English</option>
+                <option value="fr">French</option>
+                <option value="sp">Spanish</option>
+                <option value="it">Italian</option>
+                </select> <br><br>
+            </div>
+        </form>
+
 </div>
-<div>
-            <h3>To - Language</h3>
-        <select name="target_language">
-              <option value="en" selected>English</option>
-              <option value="fr">French</option>
-              <option value="sp">Spanish</option>
-              <option value="it">Italian</option>
-            </select> <br><br>
-</div>
-    </form>
 <?php require APPROOT . '/views/includes/footer.php'; ?>
