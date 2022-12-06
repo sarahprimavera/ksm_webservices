bb7dbc5a3d3e4179b81999fe2b59a186b991579a
Krikor Astour <ararad.seller@gmail.com>
2022-12-05 19:56:47 -0800

    htaccess fixed

--- webservice_finalProject/Client/app/config/config.php
+++ webservice_finalProject/Client/app/config/config.php
@@ -2,7 +2,8 @@
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/FinalProject/ksm_webservices/webservice_finalProject/Client');
+  define('URLROOT', 'http://localhost/webservice\ksm_webservices\webservice_finalProject\Client/public');
+  
   // Site Name
   define('SITENAME', 'Text Translator');
 
