30aa9aea03e6932c90a63aa0255e64c2f9db0633
sarahprimavera <sarahprimavera787@gmail.com>
2022-12-06 01:56:40 -0500

    when user signs up, it adds user into server db
    the api portion works but curl isn't being nice. needs more testing for curl

--- webservice_finalProject/Client/app/init.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    require_once 'config/config.php';
-    require_once 'core/helper.php';
-
-    spl_autoload_register(function($className){
-        require_once 'core/'. $className .'.php';
-    });
